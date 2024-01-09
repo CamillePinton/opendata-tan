44cc10d0ac1570f5b41a65818380bde2b9c73bb3
maxime-cool <gmaxime15@gmail.com>
2024-01-09 09:19:31 +0100

    added endpoint arrets

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,15 @@
 from flask import Flask
 import requests
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
+
+@app.route('/arrets', methods=['GET'])
+def get_arrets():
+    arrets = requests.get('https://open.tan.fr/ewp/arrets.json').json()
+    return arrets
