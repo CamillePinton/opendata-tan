2679668447fa4db4bf0f4dc9c6323f3e96429786
TR17 <thomas_ruan@hotmail.fr>
2024-01-09 09:17:04 +0100

    EndPoint1 : getArret

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,24 @@ from flask import Flask
 import requests
 app = Flask(__name__)
 
+open_tan="https://open.tan.fr"
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+#Recherche arrets proche d'une latitude/longitude
+@app.route('/<latitude>/<longitude>')
+def getArret(latitude, longitude):
+    req=requests.get(open_tan+"/ewp/arrets.json/{latitude}/{longitude}")
+    return req
+#Liste de tous les arrets
+
+#Horaires (théoriques)
+
+#Temps Attente
+
+#Temps attente pour un lieu ou arret et un nombre de passages
+
+#Temps attente pour un lieu ou arret, un nombre de passages et un numéro de ligne
 
